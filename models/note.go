da851d9c35e3775ec576f55fb4bd6d8e90b5d38b
Alois Carrera <aloiscarrera@gmail.com>
2020-03-29 14:14:45 -0500

    Note's data model

---
+++ models/note.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Notes struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title     string             `json:"title"`
+	Content   string             `json:"content"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+}
