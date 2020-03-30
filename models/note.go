65342e1c7fa670b70a6d5d2f965f7067474f188c
Alois Carrera <aloiscarrera@gmail.com>
2020-03-29 22:09:10 -0500

    Added user to notes model for data privacy

--- models/note.go
+++ models/note.go
@@ -7,6 +7,7 @@ import (
 
 type Notes struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	User      string             `json:"user_id"`
 	Title     string             `json:"title"`
 	Content   string             `json:"content"`
 	CreatedAt time.Time          `json:"created_at"`
