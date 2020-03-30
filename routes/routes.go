cf6f90eeb4fa885a85a86e67064f8b411170cf93
Alois Carrera <aloiscarrera@gmail.com>
2020-03-29 22:17:21 -0500

    Routes for notes C.R.U.D operations and auth middleware.

---
+++ routes/routes.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"projects/mongodb-notes-api/controllers"
+	"projects/mongodb-notes-api/models"
+)
+
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Notes API made with MongoDB & Golang",
+	})
+	return
+}
+
+func noRoute(c *gin.Context) {
+	c.JSON(http.StatusNotFound, gin.H{
+		"status":  http.StatusOK,
+		"message": "No route defined",
+	})
+}
+
+func Routes(router *gin.Engine) {
+	router.GET("/", home)
+	router.GET("/notes", models.TokenAuthMiddleware(), controllers.AllNotes)
+	router.GET("/notes/:id", models.TokenAuthMiddleware(), controllers.GetNote)
+	router.POST("/notes", models.TokenAuthMiddleware(), controllers.CreateNote)
+	router.POST("/signup", controllers.SignUp)
+	router.POST("/login", controllers.Login)
+	router.PUT("/notes/:id", models.TokenAuthMiddleware(), controllers.EditNote)
+	router.DELETE("/notes/:id", models.TokenAuthMiddleware(), controllers.DeleteNote)
+	router.NoRoute(noRoute)
+}
