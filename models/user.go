1f58b4fa291622f30e23384cc1adad6296c27fe7
Alois Carrera <aloiscarrera@gmail.com>
2020-03-29 22:10:31 -0500

    Input validation with github.com/go-playground/

--- models/user.go
+++ models/user.go
@@ -3,16 +3,16 @@ package models
 import "golang.org/x/crypto/bcrypt"
 
 type Users struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 }
 
-func (checkPass Users) HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	return string(bytes), err
 }
 
-func (checkPass Users) CheckPasswordHash(password, hash string) bool {
+func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
